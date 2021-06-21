5138e558f46d9e934aeb1aed56006bfaaed11934
Sergey Li <pincats@gmail.com>
2021-06-21 11:53:44 +0300

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "hello",
+    component: HelloComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
